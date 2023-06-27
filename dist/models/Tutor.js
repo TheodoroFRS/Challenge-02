c5a04f0e060b58396cad656f9f147cbf5d120d34
Theodoro FRS <theodoroflordarosasatyro@gmail.com>
2023-06-27 14:28:43 -0400

    starting tutor model

---
+++ dist/models/Tutor.js
@@ -0,0 +1,36 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const mongoose_1 = __importDefault(require("mongoose"));
+const TutorSchema = new mongoose_1.default.Schema({
+    name: {
+        type: String,
+        required: [true, "Please provide name"],
+        maxlength: 50,
+        minlength: 3,
+    },
+    phone: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: [true, "Please provide email"],
+        match: [
+            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+            "Please provide a valid email",
+        ],
+        unique: true,
+    },
+    date_of_birth: {
+        type: Date,
+        required: true
+    },
+    zipCode: {
+        type: Number,
+        required: true
+    }
+});
+module.exports = mongoose_1.default.model("Tutor", TutorSchema);
---
+++ src/models/Tutor.ts
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+
+const TutorSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please provide name"],
+    maxlength: 50,
+    minlength: 3,
+  },
+  phone: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: [true, "Please provide email"],
+    match: [
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+      "Please provide a valid email",
+    ],
+    unique: true,
+  },
+  date_of_birth: {
+    type: Date,
+    required: true
+  },
+  zipCode: {
+    type: Number,
+    required: true
+  }
+
+});
+
+
+module.exports = mongoose.model("Tutor", TutorSchema);
